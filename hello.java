bc5418b37391afe10a21c95f2fe5483f8211b41a
Rahul17 <rahulkumar88poi@gmail.com>
2018-08-23 21:03:04 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,14 @@
+
+
+class hello
+{
+public static void main(String args[])
+{
+System.out.print("Hello");
+
+}
+
+}
+
+
+
---
+++ name.java
@@ -0,0 +1,14 @@
+
+
+class name
+{
+public static void main(String args[])
+{
+System.out.print("Rahul");
+
+}
+
+}
+
+
+
